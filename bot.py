8f38436862560fc619c2fe82db4ead26dc28ea76
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-09-12 09:26:27 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -60,65 +60,69 @@ async def help(event):
 #bsdk credit de dena verna maa chod dege
 
 #tag
-@client.on(events.NewMessage(pattern="^/tag ?(.*)"))
+@client.on(events.NewMessage(pattern="^/tagall|@all|/all ?(.*)"))
 async def mentionall(event):
-  global moment_worker
-  if event.is_private:
-    return await event.reply("Use This In Channel or Group!")
-  
-  admins = []
-  async for admin in client.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
-    admins.append(admin.id)
-  if not event.sender_id in admins:
-    return await event.reply("Only Admin can use it [😌](https://telegra.ph/file/97da0b711a6ba2f4f4482.jpg).")
-  
-  if event.pattern_match.group(1):
-    mode = "text_on_cmd"
-    msg = event.pattern_match.group(1)
-  elif event.reply_to_msg_id:
-    mode = "text_on_reply"
-    msg = event.reply_to_msg_id
-    if msg == None:
-        return await event.reply("I can't Mention Members for Old Post!")
-  elif event.pattern_match.group(1) and event.reply_to_msg_id:
-    return await event.reply("Give me can an Argument. Ex: `/tag Hey, Where are you`")
-  else:
-    return await event.reply("Reply to Message or Give Some Text To Mention!")
-    
-  if mode == "text_on_cmd":
-    moment_worker.append(event.chat_id)
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"🫦 [{usr.first_name}](tg://user?id={usr.id})\n"
-      if event.chat_id not in moment_worker:
-        await event.respond("Ok tagger stopped [🔇](https://telegra.ph/file/97da0b711a6ba2f4f4482.jpg)")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
-        
-  
-  if mode == "text_on_reply":
-    moment_worker.append(event.chat_id)
- 
-    usrnum = 0
-    usrtxt = ""
-    async for usr in client.iter_participants(event.chat_id):
-      usrnum += 1
-      usrtxt += f"🫦 [{usr.first_name}](tg://user?id={usr.id})\n"
-      if event.chat_id not in moment_worker:
-        await event.reply("Ok tagger stopped [🔇](https://telegra.ph/file/97da0b711a6ba2f4f4482.jpg)")
-        return
-      if usrnum == 5:
-        await client.send_message(event.chat_id, usrtxt, reply_to=msg)
-        await asyncio.sleep(2)
-        usrnum = 0
-        usrtxt = ""
+    chat_id = event.chat_id
+    if event.is_private:
+        return await event.respond("__Perintah ini hanya digunakan dalam grup dan channel.__*")
 
+    is_admin = False
+    try:
+        partici_ = await client(GetParticipantRequest(
+            event.chat_id,
+            event.sender_id
+        ))
+    except UserNotParticipantError:
+        is_admin = False
+    else:
+        if (
+                isinstance(
+                    partici_.participant,
+                    (
+                            ChannelParticipantAdmin,
+                            ChannelParticipantCreator
+                    )
+                )
+        ):
+            is_admin = True
+    if not is_admin:
+        return await event.reply("__Hanya admin yang dapat menjalankan perintah ini...__")
+
+    if event.pattern_match.group(1) and event.is_reply:
+        return await event.reply("__Berikan beberapa teks atau balas pesan..__")
+    elif event.pattern_match.group(1):
+        mode = "text_on_cmd"
+        msg = event.pattern_match.group(1)
+    elif event.is_reply:
+        mode = "text_on_reply"
+        msg = await event.get_reply_message()
+        if msg == None:
+            return await event.respond(
+                "Saya tidak bisa menyebut anggota untuk pesan lama! (pesan yang dikirim sebelum saya ditambahkan ke grup)")
+    else:
+        return await event.reply("__Berikan beberapa teks atau balas pesan..__")
+
+    spam_chats.append(chat_id)
+    usrnum = 0
+    usrtxt = ''
+    async for usr in client.iter_participants(chat_id):
+        if not chat_id in spam_chats:
+            break
+        usrnum += 1
+        usrtxt += f"🫦 [{usr.first_name}](tg://user?id={usr.id})\n"
+        if usrnum == 5:
+            if mode == "text_on_cmd":
+                txt = f"{msg}\n\n{usrtxt}"
+                await client.send_message(chat_id, txt)
+            elif mode == "text_on_reply":
+                await msg.reply(usrtxt)
+            await asyncio.sleep(2)
+            usrnum = 0
+            usrtxt = ''
+    try:
+        spam_chats.remove(chat_id)
+    except:
+        pass
 
 
 print("~~~~Started~~~~~")
